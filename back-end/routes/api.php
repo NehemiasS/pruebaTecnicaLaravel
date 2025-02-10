5dcc15d623568f3a046e1e54bb421de4494bec09
NehemiasS <melkysx@gmail.com>
2025-02-10 02:49:53 -0600

    auth

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -8,5 +8,7 @@ use App\Http\Controllers\AlumnoController;
 
 Route::get('/alumno', [AlumnoController::class, 'index']);
 
-Route::post('/crear-alumno', [AlumnoController::class, 'store']);
-Route::get('/consultar-alumno/{grado}', [AlumnoController::class, 'mostrarGradoAlumno']);
+// Route::post('/crear-alumno', [AlumnoController::class, 'store']);
+// Route::get('/consultar-alumno/{grado}', [AlumnoController::class, 'mostrarGradoAlumno']);
+Route::middleware('auth.basic')->post('/crear-alumno', [AlumnoController::class, 'crear']);
+Route::middleware('auth.basic')->get('/consultar-alumno/{grado}', [AlumnoController::class, 'mostrarGradoAlumno']);
--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/crear-alumno', [AlumnoController::class, 'store']);
-Route::get('/consultar-alumno/{grado}', [AlumnoController::class, 'mostrarGradoAlumno']);
-// Route::middleware('auth.basic')->post('/crear-alumno', [AlumnoController::class, 'crear']);
-// Route::middleware('auth.basic')->get('/consultar-alumno/{grado}', [AlumnoController::class, 'mostrarGradoAlumno']);
+// Route::post('/crear-alumno', [AlumnoController::class, 'store']);
+// Route::get('/consultar-alumno/{grado}', [AlumnoController::class, 'mostrarGradoAlumno']);
+Route::middleware('auth.basic')->post('/crear-alumno', [AlumnoController::class, 'crear']);
+Route::middleware('auth.basic')->get('/consultar-alumno/{grado}', [AlumnoController::class, 'mostrarGradoAlumno']);
